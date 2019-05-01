fc0127d26e6c079c6128077e4ea0253ba18c0783
Dushyant Sheoran <dushyantsinghsheoran@gmail.com>
2019-05-01 02:17:18 -0400

    Add files via upload

---
+++ ClientGUI/CodePopUp.xaml.cs
@@ -0,0 +1,25 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//  CodePopUp.xaml.cs   -  A window to display the result or file content     //
+//  ver 1.0                                                                   //
+//  Language:         Visual C#  2017                                         //
+//  Platform:         Lenovo Z580 Windows 10                                  //
+//  Application :     FederationComm , FL17                                   //
+//  Author      :     Dushyant Sheoran, Syracuse University                   //
+//                    dsheoran@syr.edu (704)-957-2519)                        //
+////////////////////////////////////////////////////////////////////////////////
+
+using System.Windows;
+
+namespace BuildServerFederation
+{
+    /// <summary>
+    /// Interaction logic for CodePopUp.xaml
+    /// </summary>
+    public partial class CodePopUp : Window
+    {
+        public CodePopUp()
+        {
+            InitializeComponent();
+        }
+    }
+}
